a1ed9113d4ada8a49ccb72fac91846afded79aa2
Adam Reed <adam.reed@reedenterprising.com>
2022-05-07 12:53:07 -0500

    WIP

---
+++ hello-dotnet6/HelloDotNet6/HelloDotNet6.MVCWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloDotNet6.MVCWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
