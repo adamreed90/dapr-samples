ab83269e5bda48aed703150f860cd913d7df6397
georgestevens99 <georgiageorge99@gmail.com>
2022-05-07 10:55:02 -0400

    Testing to make sure our repo is still functional.

--- hello-dotnet6/HelloDotNet6/SharedLib/ServiceEndpointUrls.cs
+++ hello-dotnet6/HelloDotNet6/SharedLib/ServiceEndpointUrls.cs
@@ -1,6 +1,8 @@
 ﻿namespace SharedLib
 {
     // A comment to create a code change for git to try and compensate for a deleted branch (oops) in the git repo.
+
+    // Testing 1,2,3
     public class ServiceEndpointUrls
     {
         public const string WebAPIAhttpUrl = "http://localhost:5129";
